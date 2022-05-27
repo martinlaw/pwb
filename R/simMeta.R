1882aeb0e1a3c019a617d3898a23a3e360ebd23d
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2022-05-27 17:25:49 +0100

    simMeta now accepts a vector for N (diff't study Ns)

--- R/simMeta.R
+++ R/simMeta.R
@@ -2,7 +2,7 @@
 #'
 #' Simulates binary data for a meta-analysis.
 #'
-#' @param N sample size (shared for all trials)
+#' @param N sample size (shared for all trials if scalar)
 #' @param theta response rate (shared for all trials)
 #' @param n.studies number of trials to simulate
 #' @param nsims number of meta-analyses to simulate
@@ -11,20 +11,29 @@
 #'
 simMeta <- function(N, theta, n.studies, nsims){
 
-  responses <- vector("list", nsims)
+  if(length(N)!=1 & length(N)!=n.studies) stop("N must be length 1 (shared sample size) or length equal to n.studies")
 
+  if(length(N)==1) N <- rep(N, n.studies) # Convert N into a vector if a scalar
+
+  responses <- vector("list", nsims)
   for(i in 1:nsims){
-    responses[[i]] <- rbinom(n=n.studies, size=N, prob=theta)
+      responses[[i]] <- rbinom(n=n.studies, size=N, prob=theta)
   }
+
   responses <- do.call(rbind, responses)
 
   # Correction in case there are 0 responses or N responses:
   responses[responses==0] <- 0.5
-  responses[responses==N] <- N-0.5
+  for(i in 1:n.studies){
+    responses[responses==N[i]] <- N[i]-0.5
+  }
 
-  theta.hat <- responses/N
-  se <- sqrt((theta.hat)*(1-theta.hat)/N)
+  theta.hat <- t(apply(responses, 1, function(x) x/N))
 
+  se <- matrix(NA, nrow=nsims, ncol=n.studies)
+  for(i in 1:n.studies){
+     se[, i] <- sqrt(theta.hat[, i]*(1-theta.hat[, i])/N[i])
+  }
 
   colnames(theta.hat) <- paste("Study ", 1:n.studies, sep="")
   colnames(se) <- paste("Study ", 1:n.studies, sep="")
