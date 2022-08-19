eb51938962385e9781213c89d9cef07020e5b08b
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2022-08-19 13:09:40 +0100

    add fn for MCSE of bias

---
+++ R/MCSEbias.R
@@ -0,0 +1,15 @@
+#' Monte Carlo SE of bias
+#'
+#' Finds Monte Carlo standard error of bias for a vector of estimates.
+#'
+#' @param theta.hat vector estimated response rates
+#' @return Monte Carlo standard error of bias
+#' @export
+#'
+MCSEbias <- function(theta.hat){
+  nsim <- length(theta.hat)
+  theta.bar <- mean(theta.hat)
+  est <- sqrt(sum((theta.hat-theta.bar)^2)/((nsim-1)*nsim))
+  est
+}
+
